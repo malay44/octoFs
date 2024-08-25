0a18287a62847274fbaa7080e08593f5a30c69d9
malay44 <malay.p1@ahduni.edu.in>
2024-08-25 20:12:17 +0530

    chore: Update Input component to display error message

--- actions/createFolder.action.ts
+++ actions/createFolder.action.ts
@@ -4,35 +4,74 @@ import dbConnect from "@/lib/mongodb";
 import FileSystemItem, {
   IFileSystemDirectory,
 } from "@/models/FileSystemItem.model";
+import { mongo } from "mongoose";
 import { revalidatePath } from "next/cache";
 
-export async function createFolderAction(formData: FormData) {
-  dbConnect();
+const MongoError = mongo.MongoError;
 
-  const parentFolder = await FileSystemItem.findById(
-    formData.get("parentFolderId") as string
-  );
+export interface ICreateFolderActionState {
+  message?: string;
+  error?: any;
+  success?: boolean;
+}
 
-  if (!parentFolder || parentFolder.type !== "directory") {
-    throw new Error("Parent folder not found");
+export async function createFolderAction(
+  prevState: ICreateFolderActionState,
+  formData: FormData
+): Promise<ICreateFolderActionState> {
+  try {
+    dbConnect();
+
+    const parentFolder = await FileSystemItem.findById(
+      formData.get("parentFolderId") as string
+    );
+
+    if (!parentFolder || parentFolder.type !== "directory") {
+      return {
+        message: "Parent folder not found",
+        success: false,
+      };
+    }
+
+    const newFolder: IFileSystemDirectory = {
+      name: formData.get("name") as string,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      type: "directory",
+      parentId: parentFolder._id as string,
+      path: `${parentFolder.path}/${formData.get("name")}`,
+    };
+
+    const newFolderDocument = await new FileSystemItem(newFolder).save();
+
+    if (!parentFolder.childrenIds) {
+      parentFolder.childrenIds = [];
+    }
+    parentFolder.childrenIds.push(newFolderDocument._id as string);
+    await parentFolder.save();
+
+    revalidatePath(`/folders/${parentFolder._id}`);
+
+    return {
+      message: "Folder created successfully",
+      success: true,
+    };
+  } catch (error) {
+    if (error instanceof MongoError) {
+      if (error.code === 11000) {
+        return {
+          message: "Folder with the same name already exists",
+          success: false,
+        };
+      }
+      return {
+        message: error.message,
+        success: false,
+      };
+    }
+    return {
+      message: (error as Error)?.message || "Internal Server error occurred",
+      success: false,
+    };
   }
-
-  const newFolder: IFileSystemDirectory = {
-    name: formData.get("name") as string,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    type: "directory",
-    parentId: parentFolder._id as string,
-    path: `${parentFolder.path}/${formData.get("name")}`,
-  };
-
-  const newFolderDocument = await new FileSystemItem(newFolder).save();
-
-  if (!parentFolder.childrenIds) {
-    parentFolder.childrenIds = [];
-  }
-  parentFolder.childrenIds.push(newFolderDocument._id as string);
-  await parentFolder.save();
-
-  revalidatePath(`/folders/${parentFolder._id}`);
 }
--- components/CreateFolderPopup.tsx
+++ components/CreateFolderPopup.tsx
@@ -1,4 +1,9 @@
-import { createFolderAction } from "@/actions/createFolder.action";
+"use client";
+
+import {
+  createFolderAction,
+  ICreateFolderActionState,
+} from "@/actions/createFolder.action";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -11,7 +16,13 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import { Loader2Icon } from "lucide-react";
 import React from "react";
+import { useFormState, useFormStatus } from "react-dom";
+
+const initialState: ICreateFolderActionState = {
+  message: "",
+};
 
 export function CreateFolderPopup({
   children,
@@ -20,8 +31,20 @@ export function CreateFolderPopup({
   children: React.ReactNode;
   currentFolderId: string;
 }) {
+  const [open, setOpen] = React.useState(false);
+  const [formResponse, formAction] = useFormState(
+    createFolderAction,
+    initialState
+  );
+
+  React.useEffect(() => {
+    if (formResponse.success) {
+      setOpen(false);
+    }
+  }, [formResponse.success]);
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>{children}</DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
@@ -30,21 +53,39 @@ export function CreateFolderPopup({
             Create a new folder in the current directory.
           </DialogDescription>
         </DialogHeader>
-        <form action={createFolderAction}>
+        <form action={formAction}>
           <input type="hidden" name="parentFolderId" value={currentFolderId} />
-          <div className="py-4">
-            <Input defaultValue="New Folder" name="name" />
+          <div className="py-4 min-w-0 grid">
+            <Input
+              defaultValue="New Folder"
+              name="name"
+              error="File/Folder already Exists File/Folder already Exists File/Folder already Exists"
+            />
           </div>
           <DialogFooter>
             <DialogClose asChild>
               <Button variant="secondary">Cancel</Button>
             </DialogClose>
-            <Button type="submit" variant="secondary">
-              Save changes
-            </Button>
+            <SubmitButton />
           </DialogFooter>
         </form>
       </DialogContent>
     </Dialog>
+  );
+}
+
+function SubmitButton() {
+  const { pending: isPending } = useFormStatus();
+  return (
+    <Button type="submit" variant="secondary" disabled={isPending}>
+      {isPending ? (
+        <>
+          <Loader2Icon className="mr-2 h-4 w-4 animate-spin" />
+          Creating Folder
+        </>
+      ) : (
+        "Create Folder"
+      )}
+    </Button>
   );
 }
